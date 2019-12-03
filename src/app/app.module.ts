789dc82f039b895688b4f2b5e1dbb073d9446bb2
maidops@gmail.com <rvmp196994>
2019-12-03 14:56:11 -0300

    rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import {SidebarService} from './service/sidebar.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { IngredienteListComponent } from './ingrediente/ingrediente-list.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {HttpRequestInterceptor} from './congif/http-request.interceptor';
+import {HttpRequestInterceptor} from './config/http-request.interceptor';
 import { IngredienteFormComponent } from './ingrediente/ingrediente-form.component';
 import {MenuItem} from "primeng/api/menuitem";
 import {FormsModule} from '@angular/forms';
